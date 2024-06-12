2c1fc79a66992e971676e0b96c345621ce94daa6
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2024-06-12 17:08:23 +0530

    React Router

---
+++ ReactRouter/src/components/index.js
@@ -0,0 +1,10 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import About from "./About/About";
+import Home from "./Home/Home";
+import Contact from "./Contact/Contact";
+import User from "./User/User";
+import Github from "./Github/Github";
+import { githubInfoLoader } from "./Github/Github";
+
+export { Header, Footer, About, Home, Contact, User, Github, githubInfoLoader };
